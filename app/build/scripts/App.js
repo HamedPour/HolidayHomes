b1855bcd39a22d3e97b44a36928dc39b4b9fde79
Hamed <hamed_k.pour@yahoo.com>
2017-08-30 13:30:24 +0100

    start carousel slider

--- app/build/scripts/App.js
+++ app/build/scripts/App.js
@@ -10342,5 +10342,43 @@ var _jquery2 = _interopRequireDefault(_jquery);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+//
+// import RevealOnScroll from './modules/RevealOnScroll';//
+// new RevealOnScroll($(".SOME-CLASS-NAME"), "85%");
+//
+
+//
+var slideIndex = 1; // BABEL & WEBPACK WILL TAKE CARE OF EVERYTHING, ES5 away.
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
 /***/ })
 /******/ ]);
